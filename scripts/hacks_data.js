e7702ad6947c2ad5f2eab34d61f5175d98cee642
nashmaniac <nishantve1@gmail.com>
2015-07-14 12:26:11 +0530

    Added all hack data

--- scripts/hacks_data.js
+++ scripts/hacks_data.js
@@ -1,23 +1,5 @@
 var hack_data = [
 	{
-		image: 'images/lyricsBurst.png',
-		title: 'Lyrics burst: Visualisation',
-		subTitle: 'Made at the Music Hackday Amsterdam 2014',
-		link: 'http://lab.musixmatch.com/lyrics_burst/'
-	},
-	{
-		image: 'images/polyglot.png',
-		title: 'Polyglottis: Duolingo + Musixmatch to make foreign language playlists',
-		subTitle: 'Made at the Robot Music Hackday Bologna 2014',
-		link: 'http://lab.musixmatch.com/polyglottis3/'
-	},
-	{
-		image: 'images/narcissism2.png',
-		title: 'How narcissistic is your favourite artist?',
-		subTitle: 'Made at the Music Hackday Boston 2014',
-		link: 'http://lab.musixmatch.com/narcissism/'
-	},
-	{
 		image: 'images/lyricipsum.png',
 		title: 'Lyric Ipsum: Generate Lorem Ipsum from your favourite band\'s lyrics ',
 		subTitle: 'Made at the Music Hackday Boston 2014',
@@ -30,10 +12,22 @@ var hack_data = [
 		link: 'https://twitter.com/RhymeBustingBot'
 	},
 	{
+		image: 'images/narcissism2.png',
+		title: 'How narcissistic is your favourite artist?',
+		subTitle: 'Made at the Music Hackday Boston 2014',
+		link: 'http://lab.musixmatch.com/narcissism/'
+	},
+	{
 		image: 'images/slackBot.png',
 		title: 'mxmBot: Lyric snippets for Slack',
 		subTitle: 'Made at the weekly hackday at the Musixmatch office 2014',
 		link: 'https://twitter.com/maxciociola/status/530394433049878528'
+	},
+	{
+		image: 'images/polyglot.png',
+		title: 'Polyglottis: Duolingo + Musixmatch to make foreign language playlists',
+		subTitle: 'Made at the Robot Music Hackday Bologna 2014',
+		link: 'http://lab.musixmatch.com/polyglottis3/'
 	},
 	{
 		image: 'images/vyrical.png',
@@ -42,22 +36,92 @@ var hack_data = [
 		link: 'http://vyrical.rtrfz.com/'
 	},
 	{
-		image: 'images/slackBot.png',
-		title: 'mxmBot: Lyric snippets for Slack',
-		subTitle: 'Made at the weekly hackday at the Musixmatch office 2014',
-		link: 'https://twitter.com/maxciociola/status/530394433049878528'
+		image: 'images/lyricsBurst.png',
+		title: 'Lyrics burst: Visualisation',
+		subTitle: 'Made at the Music Hackday Amsterdam 2014',
+		link: 'http://lab.musixmatch.com/lyrics_burst/'
 	},
 	{
-		image: 'images/slackBot.png',
-		title: 'mxmBot: Lyric snippets for Slack',
+		image: 'images/imgine.png',
+		title: 'Imgine: Lyrics to images',
 		subTitle: 'Made at the weekly hackday at the Musixmatch office 2014',
-		link: 'https://twitter.com/maxciociola/status/530394433049878528'
+		link: 'http://lab.musixmatch.com/imgine/'
 	},
 	{
-		image: 'images/slackBot.png',
-		title: 'mxmBot: Lyric snippets for Slack',
+		image: 'images/playlister.png',
+		title: 'Musixmatch Curator: Playlists containing searched words',
 		subTitle: 'Made at the weekly hackday at the Musixmatch office 2014',
-		link: 'https://twitter.com/maxciociola/status/530394433049878528'
+		link: 'http://lab.musixmatch.com/playlister/'
+	},
+	{
+		image: 'images/talkify.png',
+		title: 'Talkify: Text to vocals with spotify',
+		subTitle: 'Made at the Music Hackday Paris 2014',
+		link: 'https://www.hackerleague.org/hackathons/music-hack-day-paris-number-2/hacks/talkify'
+	},
+	{
+		image: 'images/solamimi.png',
+		title: 'Solamimi: Misheard lyrics',
+		subTitle: 'Made by Hiroyuki Koike for Music Hackday Tokyo 2014',
+		link: 'http://www.plus-add.com/solamimi/'
+	},
+	{
+		image: 'images/pimpMyRadio.png',
+		title: 'Pimp my radio: Spotify + synced lyrics on a physical radio',
+		subTitle: 'Made at the Music Hackday Stockholm 2013',
+		link: 'http://evolver.fm/2013/01/23/pimp-my-radio-finally-desktop-music-hardware-for-the-21st-century/'
+	},
+	{
+		image: 'images/pebble.png',
+		title: 'lyrixPebble: Lyrics display on the Pebble smartwatch',
+		subTitle: 'Made at the Music Hackday Bologna 2013',
+		link: 'https://www.youtube.com/watch?v=VIRRUVYwM4g'
+	},
+	{
+		image: 'images/lyricIpsum.png',
+		title: 'Using a leap motion to control Musixmatch\'s spotify app',
+		subTitle: 'Made at the Music Hackday San Francisco 2013',
+		link: 'https://www.youtube.com/watch?v=Wm2U5muxK1g'
+	},
+	{
+		image: 'images/wallify.png',
+		title: 'Wallify: lyrics display across multiple phone screens',
+		subTitle: 'Made at the Music Hackday Paris 2013',
+		link: 'https://www.youtube.com/watch?v=jCgo08SsWlM'
+	},
+	{
+		image: 'images/cameraLyrics.png',
+		title: 'CameraLyrics: Take a photo and play songs about objects in the photo',
+		subTitle: 'Made at the Music Hackday Paris 2013',
+		link: 'https://www.hackerleague.org/hackathons/music-hack-day-paris-13/hacks/camera-lyrics'
+	},
+	{
+		image: 'images/johnnycash.png',
+		title: 'Johnny Cash has been everywhere (man!)',
+		subTitle: 'Made by Iain Mullan for Music Hack Day London 2012',
+		link: 'http://www.johnnycashhasbeeneverywhere.com/'
+	},
+	{
+		image: 'images/lJay.png',
+		title: 'L-Jay: Lyrics jockeying with a midi controller',
+		subTitle: 'Made at the Music Hack Day London 2012',
+		link: 'https://www.youtube.com/watch?v=re4tpJhrZf0'
+	},
+	{
+		image: 'images/cdLyrics2.png',
+		title: 'Cd barcode to lyrics',
+		subTitle: 'Made at the Music Hackday San Francisco 2011',
+		link: 'https://www.youtube.com/watch?v=ppLNOABRT0M'
+	},
+	{
+		image: 'images/wisebabblr.png',
+		title: 'Wisebabblr: online, interactive version of your artist of choice',
+		subTitle: 'Made at the Music Hackday Barcelona 2011',
+		link: 'https://drive.google.com/file/d/0B8LR2Fx_xLP3OUVTT3ZiY3M1V0NtR2dUbmlyd2tOcndTUHc0/view?usp=sharing'
 	}
+
+
+
+
 
 ];
