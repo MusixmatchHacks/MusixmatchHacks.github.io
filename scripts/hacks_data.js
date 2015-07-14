c986ed70f2c9fe8b80255d2e26a2bdae236be967
nashmaniac <nishantve1@gmail.com>
2015-07-14 11:56:01 +0530

    reordered few hacks

--- scripts/hacks_data.js
+++ scripts/hacks_data.js
@@ -1,5 +1,23 @@
 var hack_data = [
 	{
+		image: 'images/lyricsBurst.png',
+		title: 'Lyrics burst: Visualisation',
+		subTitle: 'Made at the Music Hackday Amsterdam 2014',
+		link: 'http://lab.musixmatch.com/lyrics_burst/'
+	},
+	{
+		image: 'images/polyglot.png',
+		title: 'Polyglottis: Duolingo + Musixmatch to make foreign language playlists',
+		subTitle: 'Made at the Robot Music Hackday Bologna 2014',
+		link: 'http://lab.musixmatch.com/polyglottis3/'
+	},
+	{
+		image: 'images/narcissism2.png',
+		title: 'How narcissistic is your favourite artist?',
+		subTitle: 'Made at the Music Hackday Boston 2014',
+		link: 'http://lab.musixmatch.com/narcissism/'
+	},
+	{
 		image: 'images/lyricipsum.png',
 		title: 'Lyric Ipsum: Generate Lorem Ipsum from your favourite band\'s lyrics ',
 		subTitle: 'Made at the Music Hackday Boston 2014',
@@ -12,34 +30,16 @@ var hack_data = [
 		link: 'https://twitter.com/RhymeBustingBot'
 	},
 	{
-		image: 'images/narcissism2.png',
-		title: 'How narcissistic is your favourite artist?',
-		subTitle: 'Made at the Music Hackday Boston 2014',
-		link: 'http://lab.musixmatch.com/narcissism/'
-	},
-	{
 		image: 'images/slackBot.png',
 		title: 'mxmBot: Lyric snippets for Slack',
 		subTitle: 'Made at the weekly hackday at the Musixmatch office 2014',
 		link: 'https://twitter.com/maxciociola/status/530394433049878528'
 	},
 	{
-		image: 'images/polyglot.png',
-		title: 'Polyglottis: Duolingo + Musixmatch to make foreign language playlists',
-		subTitle: 'Made at the Robot Music Hackday Bologna 2014',
-		link: 'http://lab.musixmatch.com/polyglottis3/'
-	},
-	{
 		image: 'images/vyrical.png',
 		title: 'Vyrical: Gifs synced to music and lyrics',
 		subTitle: 'Made at the weekly hackday at the Musixmatch office 2014',
 		link: 'http://vyrical.rtrfz.com/'
-	},
-	{
-		image: 'images/lyricsBurst.png',
-		title: 'Lyrics burst: Visualisation',
-		subTitle: 'Made at the Music Hackday Amsterdam 2014',
-		link: 'http://lab.musixmatch.com/lyrics_burst/'
 	},
 	{
 		image: 'images/slackBot.png',
