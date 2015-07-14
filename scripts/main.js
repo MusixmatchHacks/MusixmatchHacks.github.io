42e7fd126084e3ef379215fee921c584d236e48b
nashmaniac <nishantve1@gmail.com>
2015-07-14 12:53:42 +0530

    Added launch icon to hacks

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 (function() {
 
+	// Populate the list with hacks
 	var hack_card_template = Handlebars.compile($('#template-hackcard').html());
 	$('#hack_collection').append(hack_card_template(hack_data));
+
 
 })();
